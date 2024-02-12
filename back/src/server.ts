fe09b873357c267976af959b2c9b3a66e09afcdc
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2024-02-12 16:18:47 -0500

    added material ui and typescript support, created dynamic auction room routing

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { application, Response } from "express";
 import { z } from "zod";
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
@@ -17,98 +17,11 @@ let db = await open({
 let app = express();
 app.use(express.json({ limit: "1kb" }));
 
-let colors = ["red", "blue", "green"] as const;
-let makes = ["foo", "bar", "baaz", "quux"] as const;
-let widgetBodySchema = z.object({
-    make: z.enum(makes),
-    color: z.enum(colors),
-    manufacture_date: z.string().refine((s) => {
-        return moment(s, "YYYY-MM-DD", true).toString() !== "Invalid date";
-    }),
-    notes: z.optional(z.string()),
-});
-
-// could use this to refer to the Widget's type
-// if we wanted to try typing the request handlers
-// to ensure they returned the right things
-// type Widget = z.infer<typeof widgetBodySchema>;
-
-// if parsing fails, will return a list of string error messages
-function parseError(zodError: z.ZodError): string[] {
-    let { formErrors, fieldErrors } = zodError.flatten();
-    return [
-        ...formErrors,
-        ...Object.entries(fieldErrors).map(
-            ([property, message]) => `"${property}": ${message}`
-        ),
-    ];
-}
-
-app.get("/api/widgets/:id", async (req, res) => {
-    let { id } = req.params;
-    let result;
-    try {
-        result = await db.get("SELECT * FROM widgets WHERE id = ?", [id]);
-    } catch (err) {
-        let error = err as Object;
-        return res.status(500).json({ errors: [error.toString()] });
-    }
-    if (result === undefined) {
-        return res.status(404).json();
-    }
-    return res.json(result);
-});
-
-app.get("/api/widgets", async (req, res) => {
-    let result;
-    try {
-        result = await db.all("SELECT * FROM widgets");
-    } catch (err) {
-        let error = err as Object;
-        return res.status(500).json({ errors: [error.toString()] });
-    }
-    return res.json({ widgets: result });
-});
-
-app.post("/api/widgets", async (req, res) => {
-    let parseResult = widgetBodySchema.safeParse(req.body);
-    if (!parseResult.success) {
-        return res.status(400).json({ errors: parseError(parseResult.error) });
-    }
-    let { make, color, manufacture_date, notes } = parseResult.data;
-    let dbResult;
-    try {
-        dbResult = await db.get(
-            "INSERT INTO widgets(make, color, manufacture_date, notes) VALUES(?, ?, date(?), ?) RETURNING *",
-            [make, color, manufacture_date, notes]
-        );
-    } catch (err) {
-        let error = err as Object;
-        return res.status(500).json({ errors: [error.toString()] });
-    }
-    let { id } = dbResult;
-    res.status(201).set("Location", id).json();
-});
-
-app.delete("/api/widgets/:id", async (req, res) => {
-    let { id } = req.params;
-    let result;
-    try {
-        result = await db.get("DELETE FROM widgets WHERE id = ? RETURNING id", [
-            id,
-        ]);
-    } catch (err) {
-        let error = err as Object;
-        return res.status(500).json({ errors: [error.toString()] });
-    }
-    if (result === undefined) {
-        return res.status(404).json();
-    }
-    return res.json();
-});
-
-app.put("/api/widgets/:id", async (req, res) => {
-    // TODO
+app.post("/api/add-auction", async (req, res) => {
+    // TODO: create a proper unique ID. For now, we generate a simple unique ID by Date
+    const newAuctionId = Date.now().toString(); 
+    // TODO: Save to the database after
+    res.json({ id: newAuctionId });
 });
 
 let port = 3000;
