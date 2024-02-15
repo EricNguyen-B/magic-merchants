ff66bf181aba4c858c1bf4ee56374bd1e707f472
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2024-02-15 13:54:27 -0500

    started API for bid history table for each auction room

--- front/src/Components/AuctionRoom.tsx
+++ front/src/Components/AuctionRoom.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import BidHistoryTable from "./BidHistoryTable";
 
 const AuctionRoom = () => {
-    let { auctionId } = useParams(); 
+    let { auctionId } = useParams();
 
     return (
-        <h1>Auction Room {auctionId}</h1>
+        <>
+            <h1>Auction Room {auctionId}</h1>
+            <BidHistoryTable auctionId={auctionId!} />
+        </>
     );
 }
 
